9a182e79c5888e6b08a0cadb086b992ddb391877
dition0221 <qjatn8180@naver.com>
2023-08-31 20:55:19 +0900

    23-08-31 / React-Hook-Form(1)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,14 +1,25 @@
 import { useForm } from "react-hook-form";
 
 export default function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("name")} placeholder="Name" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("pw", {
+            required: "PW is Required",
+            minLength: { value: 5, message: "Your PW is too short" },
+          })}
+          placeholder="PW"
+        />
         <input {...register("password")} placeholder="Password" />
         <button>Add</button>
       </form>
